6d369a030151fd6a0da8cea556f86313757b4c25
mattmarchbanks <46005393+mattmarchbanks@users.noreply.github.com>
2019-01-13 14:07:05 -0700

    Project coding

---
+++ App.java
@@ -0,0 +1,33 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+// Part A
+double amountOfMoneyInWallet = 73.50;
+System.out.println("Money in my wallet: $" + amountOfMoneyInWallet);
+double itemCost = 59.50;
+System.out.println("Cost of a new video game: $" + itemCost);
+double moneyRemaining = amountOfMoneyInWallet - itemCost;
+System.out.println("Money left in wallet after purchase: $" + moneyRemaining);
+// Using decimal values other than multiples of 0.25 results in a repeating decimal answer.
+
+//Part B
+int myAge = 13;
+int numberOfFriends = 4;
+System.out.println("My total number of friends at age " + myAge + ": " + numberOfFriends);
+
+// Part C
+String firstName = "Matthew ";
+System.out.println("My first name: " + firstName);
+String lastName = "Marchbanks";
+System.out.println("My last name: " + lastName);
+String middleInitial = "A. ";
+System.out.println("My middle initial: " + middleInitial);
+System.out.println("My full name: " + firstName + middleInitial + lastName);
+
+
+	}
+
+}
